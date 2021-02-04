cf39539f4dabb09d5da4ae339e0b8d38e3072f80
Matt Heffernan <heffa2004@gmail.com>
2021-02-04 14:59:11 +0930

    removed log

--- index.js
+++ index.js
@@ -8,19 +8,13 @@ var md = require('markdown-it')({
 
 });
 
-console.log ("hello world")
 // mutation observer syntax sugar
 var arrive = require ('arrive'); 
 
 // watch for this element to exist then add markdown
 document.arrive(".description-hyperlinks.isDialogStyle", function() {
 
-    // 'this' refers to the newly created element
-    
     let note = document.querySelector('.description-hyperlinks.isDialogStyle');
-
-    // could probably add seperate attached style sheet for all the styling we want to do here.
-    // note.style = "list-style: unset;padding-left: 25px;"
 
     let text = note.innerText;
     let result = md.render(`${text}`);
