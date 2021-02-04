e215e6168e30341880032467181a3ae9734221e9
Matt Heffernan <heffa2004@gmail.com>
2021-02-04 14:55:41 +0930

    added css, removed logs and comments

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
+import './planner.css';
+
 // import the markdown module 
-var MarkdownIt = require('markdown-it'),
-md = new MarkdownIt();
-
-// mutation observer syntax sugar
-var arrive = require ('arrive') 
-
-// watch for this element to exist then add markdown
-document.arrive(".description-hyperlinks.isDialogStyle", function() {
-    // 'this' refers to the newly created element
-    
-    var note = document.querySelector('.description-hyperlinks.isDialogStyle');
-
-
-    // could probably add seperate attached style sheet for all the styling we want to do here.
-    note.style = "list-style: unset;padding-left: 25px;"
-
-    
-    var text = note.innerText;
-    var result = md.render(`${text}`);
-    note.innerHTML = result;
+var md = require('markdown-it')({
+    html: true,
+    breaks: true,
+    linkify:true
 
 });
 
+console.log ("hello world")
+// mutation observer syntax sugar
+var arrive = require ('arrive'); 
 
-// export async function render () {
+// watch for this element to exist then add markdown
+document.arrive(".description-hyperlinks.isDialogStyle", function() {
 
+    // 'this' refers to the newly created element
+    
+    let note = document.querySelector('.description-hyperlinks.isDialogStyle');
 
-// }
+    // could probably add seperate attached style sheet for all the styling we want to do here.
+    // note.style = "list-style: unset;padding-left: 25px;"
+
+    let text = note.innerText;
+    let result = md.render(`${text}`);
+    note.innerHTML = result;
+
+});
