837fc71596411626f6db5a97d690657a8e07631e
MattHeffNT <heffa2004@gmail.com>
2022-03-16 21:59:45 +1100

    udpate html element target

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ const md = require('markdown-it')({
     linkify:true
 });
 
-console.log (`planner markdown extension Icons made by https://www.flaticon.com/authors/prosymbols"Prosymbols from "https://www.flaticon.com/"`)
+//planner markdown extension Icons made by https://www.flaticon.com/authors/prosymbols"Prosymbols from "https://www.flaticon.com/
+
 
 // mutation observer syntax sugar
 const arrive = require ('arrive'); 
 
 // watch for this element to exist then add markdown
-document.arrive(".description-hyperlinks.isDialogStyle", function() {
+document.arrive(".description-hyperlinks", function() {
 
      // The note container where comments 
-    let note = document.querySelector('.description-hyperlinks.isDialogStyle');
+    let note = document.querySelector('.description-hyperlinks');
     let text = note.innerText;
     let result = md.render(`${text}`);
     note.innerHTML = result;
