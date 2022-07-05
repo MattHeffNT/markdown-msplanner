682488ac0f659d66cc67a8fc55f48c9502dcf2f9
Matt <heffa2004@gmail.com>
2022-07-05 14:30:03 +1000

    lists working again

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ import './planner.css';
 
 // import the markdown module 
 const md = require('markdown-it')({
-    html: true,
+    // html: true,
     breaks: true,
-    linkify:true
+    linkify: true
 });
 
 //planner markdown extension Icons made by https://www.flaticon.com/authors/prosymbols"Prosymbols from "https://www.flaticon.com/
 
 
 // mutation observer syntax sugar
-const arrive = require ('arrive'); 
+const arrive = require('arrive');
 
 // watch for this element to exist then add markdown
-document.arrive(".description-hyperlinks", function() {
+document.arrive(".description-hyperlinks", function () {
 
-     // The note container where comments 
+    // The note container where comments 
     let note = document.querySelector('.description-hyperlinks');
     let text = note.innerText;
     let result = md.render(`${text}`);
